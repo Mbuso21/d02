db76c3047be41a2ffc724a35b6a96daa912578e0
Mbuso Mehlomakulu <mmehloma@c5r10s10.wethinkcode.co.za>
2020-06-21 16:32:24 +0200

    ex05

--- ex05/ft_print_comb2.c
+++ ex05/ft_print_comb2.c
@@ -6,8 +6,59 @@
 /*   By: mmehloma <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/21 15:25:45 by mmehloma          #+#    #+#             */
-/*   Updated: 2020/06/21 15:25:58 by mmehloma         ###   ########.fr       */
+/*   Updated: 2020/06/21 16:29:58 by mmehloma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include 
+#include <unistd.h>
+
+void ft_putchar(char t)
+{
+	write(1, &t, sizeof(char));
+}
+
+void ft_print_comb2(void)
+{
+	char th, h, t, d;
+
+	th = '0';
+
+	
+
+	while(th <= '9')
+	{
+		h = th + 0;
+		while(h <= '8')
+		{
+			t = h + 0;
+			while(t <= '9')
+			{
+				d = t - 1;
+				while(d <= '9')
+				{
+					if (th != h)
+					{
+
+					ft_putchar(th);
+					ft_putchar(h);
+					ft_putchar(' ');
+					ft_putchar(t);
+					ft_putchar(d);
+					ft_putchar(',');
+					}
+				d++;
+				}
+			t++;
+			}
+		h++;
+		}
+	th++;
+	}
+}
+
+int main(void)
+{
+	ft_print_comb2();
+	return 0;
+}	
+
